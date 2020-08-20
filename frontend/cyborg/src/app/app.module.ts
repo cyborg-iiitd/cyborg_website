2b30d6d236db2b3c0567f5eff3a5aa0852b0b8a0
Tushar <tushar.mohan2001@gmail.com>
2020-08-21 03:33:51 +0530

    cards added

--- frontend/cyborg/src/app/app.module.ts
+++ frontend/cyborg/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { BlogComponent } from './blog/blog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
