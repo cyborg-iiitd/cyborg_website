95d68d8d9c6030b04203151996661841177f29f9
Tushar <tushar.mohan2001@gmail.com>
2020-08-23 00:52:27 +0530

    alignment of footer

--- frontend/cyborg/src/app/footer/footer.component.ts
+++ frontend/cyborg/src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import $ from "jquery";
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,6 +12,18 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function() {
+      if($(window).width() <= 800){
+        $("ul").removeClass("container");
+        $("ul").removeClass("align");
+        $("ul").addClass("container");
+      }
+      else{
+        $("ul").removeClass("container");
+        $("ul").removeClass("align");
+        $("ul").addClass("align");
+      }
+    });
   }
 
 }
