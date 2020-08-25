5b65d7dc2d474145175cfb857439fab91e4a404a
Tushar <tushar.mohan2001@gmail.com>
2020-08-26 03:41:26 +0530

    projects page

--- frontend/cyborg/src/app/app-routing.module.ts
+++ frontend/cyborg/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: "", component:HomeComponent},
   {path: "about", component:AboutComponent},
   {path: "projects", component:BlogComponent},
-  {path: "about#feedback", component:FeedbackComponent}
+  
 ];
 
 @NgModule({
