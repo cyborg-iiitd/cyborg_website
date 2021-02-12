31088e60cd9970715f4d37be0413ab7205f35c1c
tushar2407 <tushar.mohan2001@gmail.com>
2021-02-13 01:13:37 +0530

    asomer

---
+++ docs/assets/project_data.ts
@@ -0,0 +1,16 @@
+export const Projects_data=[
+    {
+        "Title": "Drone Simulator",
+        "Description": "Simulation of a parameterized quadrotor, implementing the PID Control Algorithm. Has a fully functioning implementation of drone mechanics, and hence can serve as a test bed for other control strategies as well. ",
+        "Drive" : "#",
+        "Github": "https://github.com/cyborg-iiitd/Drone_Simulator ",
+        "Image": "https://drive.google.com/uc?export=view&id=1mwosYFVIJkD-XeIcrPte7X9_CenzXnrq",
+        "MDFile": "https://raw.githubusercontent.com/cyborg-iiitd/Drone_Simulator/master/README.md",
+        "Members":[
+            "Devansh",
+            "Mudit "
+        ]
+    }
+]
+// https://drive.google.com/uc?export=view&id=1MiGisIl72qyN0wJD6nGyvS0exiJp0tz7
+// https://drive.google.com/file/d//view?usp=sharing
