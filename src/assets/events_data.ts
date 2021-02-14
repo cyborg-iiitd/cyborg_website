4701549cb323dc3534a4bfa765e020774c7e1f22
Dipanshu <dipanshu18139@iiitd.ac.in>
2021-02-14 13:17:17 +0530

    Projects Updated

--- src/assets/events_data.ts
+++ src/assets/events_data.ts
@@ -1,5 +1,13 @@
 export const Event_data=[
     {
+        "Title":"Introduction to Control Theory: PID Controller ",
+        "Description":"An introduction to Control Theory and its numerous applications.The first session in series was on PID (proportional–integral–derivative) controls. Starting with a very simple model, we build up to real life applications like drone flying.",
+        "Drive":"https://drive.google.com/drive/folders/1cClnf2w89DIx7YGE6G2eAES-QdNphrzQ?usp=sharing",
+        "Github":"#",
+        "Image":"https://drive.google.com/uc?export=view&id=1tqf-cLjtLCaULh9fLDe6wJUfVYHxn3o1",
+        "Date": "02/09/2020"
+    },
+    {
         "Title": "Maze Coverage and Basics of Graphs",
         "Description": "Discussion on maze-solving, pathfinding algorithms. A head start for some important topics in CSE courses as well as MTH courses for freshers, especially for coding your own algorithm to make your bots work.",
         "Drive" : "https://drive.google.com/drive/folders/1ph1EWYhNPRh9bB6NK3bM4A2R_tHZnUUc?usp=sharing",
@@ -14,13 +22,5 @@ export const Event_data=[
         "Github": "#",
         "Image": "https://drive.google.com/uc?export=view&id=10lEqTq0JCGXBcjmJXDf-ZqoYrCYenplk",
         "Date" : "25/09/2019"   
-    },
-    {
-        "Title":"Introduction to Control Theory: PID Controller ",
-        "Description":"An introduction to Control Theory and its numerous applications.The first session in series was on PID (proportional–integral–derivative) controls. Starting with a very simple model, we build up to real life applications like drone flying.",
-        "Drive":"https://drive.google.com/drive/folders/1cClnf2w89DIx7YGE6G2eAES-QdNphrzQ?usp=sharing",
-        "Github":"#",
-        "Image":"https://drive.google.com/uc?export=view&id=1tqf-cLjtLCaULh9fLDe6wJUfVYHxn3o1",
-        "Date": "02/09/2020"
     }
 ]
